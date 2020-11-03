c693e85231048ce46bc4070c995bdd2b96f7bad9
Simei Yeh <simei.yeh@gmail.com>
2020-11-03 00:28:10 -0800

    Updated challenge 1 and started challenge 2

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -6,11 +6,12 @@ var firstPlayer = "X";
 var currentPlayer = firstPlayer;
 var Xwins = 0;
 var Owins = 0;
+var gamePieces = 0;
 
 //toggle Players
-var togglePlayer = function(currentPlayer) {
-  if (currentPlayer === "X") {
-    console.log('toggleplayer',currentPlayer);
+var togglePlayer = function (player) {
+  console.log('toggleplayer', currentPlayer);
+  if (player === "X") {
     currentPlayer = "O";
   } else {
     currentPlayer = "X"
@@ -18,15 +19,38 @@ var togglePlayer = function(currentPlayer) {
 }
 
 //function to check if one player won 3 in a row
-var checkWinCondition = function() {
-  for (let i = 1; i <= 9; i++) {
+var checkWinCondition = function (player) {
+  let won = false;
+  let array = [1, 2, 3];
 
+  var checkWins = function (count, possArray) {
+    //base case
+    if (count === 3) {
+      won = true;
+      return;
+    }
+
+    for (let i = 1; i < 4; i++) {
+      for (let j = 1; j < 4; j++) {
+        let currentBox = document.getElementById('box' + (i * j)).innerHTML;
+        console.log(i, j, count, currentBox)
+        if (currentBox) {
+          if (player === currentBox) {
+            count++;
+          }
+          checkWins(count);
+          count--;
+        }
+      }
+    }
   }
+  checkWins(0);
+  return won;
 }
 
 //clear all moves
-document.getElementById("clear").addEventListener("click", function() {
-  for (let i = 1 ; i <= 9; i++) {
+document.getElementById("clear").addEventListener("click", function () {
+  for (let i = 1; i <= 9; i++) {
     document.getElementById("box" + i).innerHTML = "";
   }
 })
@@ -34,17 +58,21 @@ document.getElementById("clear").addEventListener("click", function() {
 
 //add native event listener to each box
 
-for (let i = 1 ; i <= 9; i++) {
-  document.getElementById("box" + i).addEventListener("click", function() {
+for (let i = 1; i <= 9; i++) {
+  document.getElementById("box" + i).addEventListener("click", function () {
     document.getElementById("box" + i).innerHTML = currentPlayer;
+    gamePieces++;
+    if (gamePieces >= 5 && checkWinCondition(currentPlayer)) {
+      alert(currentPlayer + ' wins! Reset game board.');
+      currentPlayer === 'X' ? Xwins++ : Owins++;
+    };
     togglePlayer(currentPlayer);
-    checkWinCondition();
   })
 }
 
 //present total wins by player
 
-document.getElementById("clear").addEventListener("click", function() {
+document.getElementById("clear").addEventListener("click", function () {
   document.getElementById("winsCount").innerHTML =
     "X wins  " + Xwins + " and   O wins  " + Owins;
 })
