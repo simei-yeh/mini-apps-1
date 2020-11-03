be67b898802097ba298f1ec760d0ad577172f829
Simei Yeh <simei.yeh@gmail.com>
2020-11-02 18:40:59 -0800

    Progress on mini app 1

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,44 @@
+//first move starts with X
+//app detects win/tie and displays appropriate message
+//button resets the game
+
+var firstPlayer = "X";
+var currentPlayer = firstPlayer;
+
+//toggle Players
+var togglePlayer = function(currentPlayer) {
+  if (currentPlayer === "X") {
+    console.log('toggleplayer',currentPlayer);
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X"
+  }
+}
+
+//function to check if one player won 3 in a row
+var checkWinCondition = function() {
+  for (let i = 1; i <= 9; i++) {
+
+  }
+}
+
+//clear all moves
+document.getElementById("clear").addEventListener("click", function() {
+  for (let i = 1 ; i <= 9; i++) {
+    document.getElementById("box" + i).innerHTML = "";
+  }
+})
+
+
+//add native event listener to each box
+
+for (let i = 1 ; i <= 9; i++) {
+  document.getElementById("box" + i).addEventListener("click", function() {
+    document.getElementById("box" + i).innerHTML = currentPlayer;
+    togglePlayer(currentPlayer);
+  })
+}
+
+
 
 
